376429f1d3aa8eb8b27ce7febfd81f4fc378eb1b
Rafly-Engineer <muhammad_rachman_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 00:07:42 -0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,8 @@
+public class Tes {
+    public static void main(String[] args) {
+        System.out.println("nama saya Rafly dari kelas 10 X RPL 7 yang berasal dari kota sidoarjo");
+        String a = "1"; 
+    }
+
+    
+}
