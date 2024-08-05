f3b83147af3c117f722243ef07a449a59b4cf7b4
Rafly-Engineer <muhammad_rachman_33rpl@student.smktelkom-mlg.sch.id>
2024-08-04 22:42:57 -0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,15 @@
+public class data {
+    public static void main(String[] args) {
+         //tipe_data nama_var = value/isi var;
+         String nama = "Rafly";
+         int usia = 15;
+         String alamat = "Pondok Wage Indah 2";
+         double kelas = 10.7;
+ 
+         System.out.println("nama saya: " + nama);
+         System.out.println("usia : " + usia);
+         System.out.println("alamat : " + alamat);
+         System.out.println("kelas : " + kelas);
+ 
+}
+}
